ad952faafde609a9abcc5d291753deb9c88b67b5
Karim Messaoud <karim7@wp.pl>
2022-05-13 10:57:27 +0200

    Added Book and Video models

---
+++ ClaramontanaBibliography.Data/Models/LibraryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaramontanaBibliography.Data.Models
+{
+    public abstract class LibraryItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ClaramontanaBibliography.Data/Models/Video.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaramontanaBibliography.Data.Models
+{
+    public class Video : LibraryItem
+    {
+        public string Director { get; set; }
+    }
+}
