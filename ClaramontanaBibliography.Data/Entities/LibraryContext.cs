769f691bef64fe9e3d7e967b776ab5b5edb877ba
Karim Messaoud <karim7@wp.pl>
2022-05-18 13:18:52 +0200

    Added Users table

--- ClaramontanaBibliography.Data/Entities/LibraryContext.cs
+++ ClaramontanaBibliography.Data/Entities/LibraryContext.cs
@@ -16,5 +16,6 @@ namespace ClaramontanaBibliography.Data.Entities
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Video> Videos { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
