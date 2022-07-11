861b551eb4ad673fb78b9def18036b4f46713288
Karim Messaoud <karim7@wp.pl>
2022-07-11 15:43:33 +0200

    Add ObjectMapper class to handle mappings with Automapper in the ClaramontanaOnlineShop.Service project

---
+++ ClaramontanaOnlineShop.Service/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ClaramontanaOnlineShop.Data.Entities;
+
+
+namespace ClaramontanaOnlineShop.Service
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, Product>();
+            // Additional mappings here...
+        }
+    }
+}
